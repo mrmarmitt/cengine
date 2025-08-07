1641f989cb9fc5793e8b4450fcf19bc1a67c5637
Mateus Rufino Marmitt <mrmarmitt@gmail.com>
2025-08-07 16:46:21 -0300

    removing build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mrmar/Documents/projetos_de_estudo/c++/cengine
-# Build directory: C:/Users/mrmar/Documents/projetos_de_estudo/c++/cengine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/test")
